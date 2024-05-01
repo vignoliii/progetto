294efa37fb2ae1acf834ef4b0c921fc1fc5ce228
vignoliii <168664936+vignoliii@users.noreply.github.com>
2024-05-01 18:57:03 +0200

    Create Boid.hpp

---
+++ codio/Boid.hpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "Vector.hpp"
+#include <vector>
+#include <stdlib.h>
+
+#ifndef Boid_HPP
+#define Boid_HPP
+class Boid{
+    public:
+    
+Vector coordinates;
+Vector speed;
+
+//Costruttore?
+Boid(float x,float y);
+//Boid();
+
+//Rules
+Vector Allignment(const std::vector <Boid> ensamble);    //const fuori??
+Vector Cohesion(const std::vector <Boid> ensamble);     //const fuori??
+Vector Separation(const std::vector <Boid> ensamble);   //const fuori??
+void Apply_rules(const std::vector <Boid> ensamble )
+;
+void Pacman_effect(int window_x,
+int window_y);
+void Play(const std::vector <Boid> ensamble);
+
+};
+#endif
