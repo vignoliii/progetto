620148991721ab21e42741b9c55b5a099228e5ce
vignoliii <168664936+vignoliii@users.noreply.github.com>
2024-05-07 17:51:28 +0200

    Create main.cpp

---
+++ codio/main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include "B_Simulation.hpp"
+#include "SFML/Window.hpp"
+#include "SFML/Graphics.hpp"
+int main(){
+
+
+    Simulation Simulazione;
+    Simulazione.Run_Simulation();
+    return 0;
+
+}
